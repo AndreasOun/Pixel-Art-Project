9c5a9f851c2a41243fbc67fee73c9331731b718f
AndreasOun <andreastthk@gmail.com>
2023-04-26 02:02:19 +0300

    Fixed margins and padding

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 const container = document.querySelector('#container');
+const controls = document.querySelector('#controls');
+const btn16 = document.querySelector('#btn-16');
+const btn32 = document.querySelector('#btn-32');
+const btn64 = document.querySelector('#btn-64');
+
+// create initial grid
+createGrid(16);
+
+// add event listeners to buttons
+btn16.addEventListener('click', () => {
+	removeGrid();
+	createGrid(16);
+});
+btn32.addEventListener('click', () => {
+	removeGrid();
+	createGrid(32);
+});
+btn64.addEventListener('click', () => {
+	removeGrid();
+	createGrid(64);
+});
+
+// function to create grid
+function createGrid(size) {
+    // calculate size of each grid square based on container size and number of squares
+    const squareSize = size === 16 ? 60 : size === 32 ? 30 : 15;
+  
+    // set width and height of container based on number of squares and square size
+    const containerSize = size * squareSize;
+    container.style.width = `${containerSize}px`;
+    container.style.height = `${containerSize}px`;
+  
+    // create grid squares and add to container
+    for (let i = 0; i < size * size; i++) {
+      const div = document.createElement('div');
+      div.classList.add('grid-square');
+      div.style.width = `${squareSize}px`;
+      div.style.height = `${squareSize}px`;
+      container.appendChild(div);
+    }
+  
+    // add event listener to each grid square
+    const squares = document.querySelectorAll('.grid-square');
+    squares.forEach((square) => {
+      square.addEventListener('mouseover', () => {
+        square.classList.add('active');
+      });
+    });
+  }
+// function to remove old grid
 
 
-//creates grid squares and add to container
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-square');
-    container.appendChild(div);
+function removeGrid() {
+	const squares = document.querySelectorAll('.grid-square');
+	squares.forEach(square => {
+		container.removeChild(square);
+	});
 }
 
-//event listener for each grid square
-const squares = document.querySelectorAll('.grid-square');
-squares.forEach(square => {
-	square.addEventListener('mouseover', () => {
-		square.classList.add('active');
-	});
-});
+
+
+
