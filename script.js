8ce1519b7928978c896d79b5467f1bee6036130c
AndreasOun <andreastthk@gmail.com>
2023-04-26 01:20:59 +0300

    Added DOM manipulation

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const container = document.querySelector('#container');
 
+
+//creates grid squares and add to container
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-square');
     container.appendChild(div);
 }
+
+//event listener for each grid square
+const squares = document.querySelectorAll('.grid-square');
+squares.forEach(square => {
+	square.addEventListener('mouseover', () => {
+		square.classList.add('active');
+	});
+});
